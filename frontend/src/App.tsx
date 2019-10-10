64b75783b460d12a72e022e548762222fb581d41
tzaeru <tzaeru@gmail.com>
2019-10-10 22:12:18 +0300

    Landing page work, clearing some structures

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,34 +24,34 @@ const App: React.FC = () => {
 
 	// 						<Link to="/palaute?id=985507e2-735f-48a9-a941-75b38f0e4adb">Hoivakodit</Link>
 	return (
-		    <Provider unstable_system={system}>
+		<Provider unstable_system={system}>
 
-		<div className="app">
-			<Router>
-				<header className="app-header">
-					<div id="app-name"><Link to="/">Espoon Hoivakodit</Link></div>
+			<div className="app">
+				<Router>
+					<header className="app-header">
+						<div id="app-name"><Link to="/">Espoon Hoivakodit</Link></div>
 
-					<div id="app-links">
-						<Link to="/hoivakodit">Hoivakodit</Link>
-						<Link to="/">Hakeminen</Link>
-						<Link to="/">Tietoa Palvelusta</Link>
+						<div id="app-links">
+							<Link to="/hoivakodit">Hoivakodit</Link>
+							<Link to="/">Hakeminen</Link>
+							<Link to="/">Tietoa Palvelusta</Link>
+						</div>
+					</header>
+
+					<div id="app-banner">
+						<img width="100%" src={process.env.PUBLIC_URL + "person-elderly.jpg"} />
 					</div>
-				</header>
 
-				<div id="app-banner">
-					<img width="100%" src={process.env.PUBLIC_URL + "person-elderly.jpg"} />
-				</div>
+					<div>
+						<Route exact path="/" component={Landing} />
+						<Route path="/hoivakodit" component={NurseryHomes} />
+						<Route path="/topics" component={NurseryHomes} />
+						<Route path="/palaute" component={Feedback} />
+					</div>
+				</Router>
+			</div>
 
-				<div>
-					<Route exact path="/" component={Landing} />
-					<Route path="/hoivakodit" component={NurseryHomes} />
-					<Route path="/topics" component={NurseryHomes} />
-					<Route path="/palaute" component={Feedback} />
-				</div>
-			</Router>
-		</div>
-
-		    </Provider>
+		</Provider>
 
 	)
 }
--- frontend/src/landing.tsx
+++ frontend/src/landing.tsx
@@ -10,31 +10,45 @@ import {
 	MenuSeparator
 } from "reakit/Menu";
 
+import { Button } from "reakit/Button";
+
 
 function Landing() {
 
 	const menu = useMenuState();
 
+	const areas = ["Espoon Keskus", "Espoonlahti", "Leppävaara", "Matinkylä", "Tapiola"];
+
+	let selected_area = "";
+
+	const on_selected_area = function(event: any) {
+		console.log(event.target.value)
+		selected_area = event.target.value;
+	}
+
+	const menu_items_dom: object[] = areas.map((area) => {
+		return (
+ 			<option value={area}>{area}</option>
+		)
+	});
+
 	return (
 		<div id="landing">
-			<b>Miltä alueelta etsit hoivakotia?</b>
+			<div id="pick-nursery">
+				<b>Miltä alueelta etsit hoivakotia?</b>
+				<select onChange={on_selected_area}>
+					{menu_items_dom}
+				</select>
 
-			      <MenuDisclosure {...menu}>Menu</MenuDisclosure>
-      <Menu {...menu} aria-label="Example">
-        <MenuItem
-          {...menu}
-          onClick={() => {
-            menu.hide();
-            console.log("clicked on button");
-          }}
-        >
-          Button
-        </MenuItem>
-        <MenuItem {...menu} as="a" href="#" onClick={menu.hide}>
-          Link
-        </MenuItem>
-      </Menu>
+				<Button onClick={() => alert("clicked")}>
+					Näytä Hoivakodit
+				</Button>
+			</div>
+			<div id="info">
+				Miten saan tehostetun palveluasumisen?
 
+				Mitä on tehostettu blah blah?
+			</div>
 		</div>
 	)
 }
