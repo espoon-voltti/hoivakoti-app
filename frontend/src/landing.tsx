0a0d28a16e862f60aa6f2bfa635cf115490c97bd
tzaeru <tzaeru@gmail.com>
2019-10-11 21:03:36 +0300

    Proper linking from landing to listing of nursing homes.

--- frontend/src/landing.tsx
+++ frontend/src/landing.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import "./landing.css"
+import { withRouter, Redirect } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 import {
 	useMenuState,
@@ -14,21 +16,22 @@ import { Button } from "reakit/Button";
 
 
 function Landing() {
+	let history = useHistory();
 
 	const menu = useMenuState();
 
 	const areas = ["Espoon Keskus", "Espoonlahti", "Leppävaara", "Matinkylä", "Tapiola"];
 
-	let selected_area = "";
+	let selected_area = -1;
 
 	const on_selected_area = function(event: any) {
 		console.log(event.target.value)
 		selected_area = event.target.value;
 	}
 
-	const menu_items_dom: object[] = areas.map((area) => {
+	const menu_items_dom: object[] = areas.map((area, index) => {
 		return (
- 			<option value={area}>{area}</option>
+			<option value={index}>{area}</option>
 		)
 	});
 
@@ -39,15 +42,19 @@ function Landing() {
 				<select onChange={on_selected_area}>
 					{menu_items_dom}
 				</select>
-
-				<Button onClick={() => alert("clicked")}>
+				<Button onClick={() => {
+					const url = "/hoivakodit" + (selected_area >= 0 ? "&hk=" + selected_area : "");
+					history.push(url);
+				}}>
 					Näytä Hoivakodit
 				</Button>
 			</div>
 			<div id="info">
-				Miten saan tehostetun palveluasumisen?
-
+				Miten saan tehostetun palveluasumisen? - tälle css:ään joku kiva header-stylaus
+				<br/>
 				Mitä on tehostettu blah blah?
+				<br/>
+				Palvelulupaus
 			</div>
 		</div>
 	)
