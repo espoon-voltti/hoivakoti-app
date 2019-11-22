599f508e749ca42f476945d4ad4acfe51fc8a953
ak05 <ak05@L1421>
2019-11-22 16:55:39 +0200

    Implement white layout styles for react-images

--- frontend/src/components/Lightbox.tsx
+++ frontend/src/components/Lightbox.tsx
@@ -17,12 +17,53 @@ interface Props {
 	images: ImageView[];
 }
 
+const stylesModal = {
+	blanket: (base: any) => ({
+		...base,
+		backgroundColor: "rgba(255,255,255,0.85)",
+	}),
+};
+
+const stylesCarousel = {
+	header: (base: any) => ({
+		...base,
+		background: "none !important",
+	}),
+	headerClose: (base: any) => ({
+		...base,
+		color: "#666",
+		":hover": { color: "#DE350B" },
+	}),
+	footer: (base: any) => ({
+		...base,
+		background: "none !important",
+	}),
+	footerCaption: (base: any) => ({
+		...base,
+		color: "black",
+	}),
+	navigationNext: (base: any) => ({
+		...base,
+		color: "#666",
+		":hover": { color: "#DE350B" },
+	}),
+	navigationPrev: (base: any) => ({
+		...base,
+		color: "#666",
+		":hover": { color: "#DE350B" },
+	}),
+};
+
 const Lightbox: FC<Props> = ({ isOpen, onClose, images }) => {
 	return (
 		<ModalGateway>
 			{isOpen ? (
-				<Modal onClose={onClose} allowFullscreen={false}>
-					<Carousel views={images} />
+				<Modal
+					onClose={onClose}
+					allowFullscreen={false}
+					styles={stylesModal}
+				>
+					<Carousel views={images} styles={stylesCarousel} />
 				</Modal>
 			) : null}
 		</ModalGateway>
