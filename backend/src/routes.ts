c39f78a3ee91e9743d2eedc72c4f1dbe66c64912
tzaeru <tzaeru@gmail.com>
2019-11-26 14:03:57 +0200

    Accidentally copypasted something silly to the file

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -114,8 +114,6 @@ router.get("/api/test-match/:pass", async ctx => {
 	ctx.body = ctx.params.pass === process.env.ADMIN_PASSWORD;
 });
 
-62 {var.region}:${data.aws_caller_identity.current.account_id}:parameter/${terraform.workspace}/hoivakoti/admin_password"
-
 const routes = router.routes();
 
 export { routes };
