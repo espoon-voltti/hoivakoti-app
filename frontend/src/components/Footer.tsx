234d0a083a48812da940a3d833acab011b1e5d36
ak05 <ak05@L1421>
2019-11-27 06:56:09 +0200

    Add sv-FI localized link targets to footer items

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,49 +1,63 @@
 import React, { FC } from "react";
 import config from "./config";
 import { Link } from "react-router-dom";
-import { useT } from "../translations";
+import { useT, useCurrentLanguage } from "../translations";
 
-const Footer: FC = () => (
-	<footer
-		className="header footer"
-		style={{ borderTop: "1px solid #e8e8e8" }}
-	>
-		<div className="logo-container">
-			<a href="/">
-				<img
-					className="logo"
-					src={config.PUBLIC_FILES_URL + "/logo-espoo.svg"}
-					alt="Espoo logo"
-				/>
-			</a>
-		</div>
+const Footer: FC = () => {
+	const currentLanguage = useCurrentLanguage();
 
-		<nav className="footer-links">
-			<span className="footer-link">
-				<a
-					href="https://www.espoo.fi/fi-FI/Asioi_verkossa/Tietosuoja/Tietosuojaselosteet"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					{useT("footerLinkPrivacy")}
+	const linkPrivacy =
+		currentLanguage === "sv-FI"
+			? "https://www.esbo.fi/sv-FI/Etjanster/Dataskydd/Dataskyddsbeskrivningar"
+			: "https://www.espoo.fi/fi-FI/Asioi_verkossa/Tietosuoja/Tietosuojaselosteet";
+
+	const linkFeedback =
+		currentLanguage === "sv-FI"
+			? "https://easiointi.espoo.fi/eFeedback/sv"
+			: "https://easiointi.espoo.fi/eFeedback/fi/Home";
+
+	return (
+		<footer
+			className="header footer"
+			style={{ borderTop: "1px solid #e8e8e8" }}
+		>
+			<div className="logo-container">
+				<a href="/">
+					<img
+						className="logo"
+						src={config.PUBLIC_FILES_URL + "/logo-espoo.svg"}
+						alt="Espoo logo"
+					/>
 				</a>
-			</span>
-			<span className="footer-link">
-				<Link to="/saavutettavuus">
-					{useT("footerLinkAccessibility")}
-				</Link>
-			</span>
-			<span className="footer-link">
-				<a
-					href="https://easiointi.espoo.fi/eFeedback/fi/Home"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					{useT("footerLinkFeedback")}
-				</a>
-			</span>
-		</nav>
-	</footer>
-);
+			</div>
+
+			<nav className="footer-links">
+				<span className="footer-link">
+					<a
+						href={linkPrivacy}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{useT("footerLinkPrivacy")}
+					</a>
+				</span>
+				<span className="footer-link">
+					<Link to="/saavutettavuus">
+						{useT("footerLinkAccessibility")}
+					</Link>
+				</span>
+				<span className="footer-link">
+					<a
+						href={linkFeedback}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{useT("footerLinkFeedback")}
+					</a>
+				</span>
+			</nav>
+		</footer>
+	);
+};
 
 export default Footer;
