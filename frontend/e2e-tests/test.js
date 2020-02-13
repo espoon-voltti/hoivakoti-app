f2aac7460ad2c09fa67241340a4ac8b3b46ebabd
Antti Virtanen <antti.virtanen@solinor.com>
2020-02-13 14:25:30 +0200

    Simple Nightwatch test

--- frontend/e2e-tests/test.js
+++ frontend/e2e-tests/test.js
@@ -1,13 +1,7 @@
 module.exports = {
-  'Demo test Google' : function (browser) {
+  'Demo test' : function (browser) {
     browser
-      .url('http://localhost:4000')
-      .waitForElementVisible('body')
-      .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('input[name=btnK]')
-      .click('input[name=btnK]')
-      .pause(1000)
-      .assert.containsText('#main', 'Night Watch')
-      .end();
+      .url('http://localhost:4000/hoivakodit')
+      .waitForElementVisible('.card-container')
   }
 };
