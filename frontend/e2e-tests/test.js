83b608ee58163ec9d56a99d2de2a9b33eeb761c4
Antti Virtanen <antti.virtanen@solinor.com>
2020-02-13 15:23:49 +0200

    ci script fix

--- frontend/e2e-tests/test.js
+++ frontend/e2e-tests/test.js
@@ -2,6 +2,6 @@ module.exports = {
   'Demo test' : function (browser) {
     browser
       .url('http://localhost:4000/hoivakodit')
-      .waitForElementVisible('.card-container')
+      .waitForElementVisible('.card-container', 30000)
   }
 };
