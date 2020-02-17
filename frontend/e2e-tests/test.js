f2bb363c144ec6d1195130a9c0ef201cbebea8b4
Antti Virtanen <antti.virtanen@solinor.com>
2020-02-17 10:37:20 +0200

    updated git-ignore and e2e-tests ip to 0.0.0.0

--- frontend/e2e-tests/test.js
+++ frontend/e2e-tests/test.js
@@ -1,15 +1,43 @@
 module.exports = {
-  'Demo test' : function (browser) {
+  'React is can display /hoivakodit template' : function (browser) {
     browser
-      .pause(5000)
-      .url('http://127.0.0.1:4000/hoivakodit')
+      .url('http://0.0.0.0:4000/hoivakodit')
       .waitForElementVisible('.results-summary')
       .end()
   },
-  'Demo test 1' : function (browser) {
+  'React can get and display content from backend' : function (browser) {
     browser
-      .url('http://127.0.0.1:4000/hoivakodit')
+      .url('http://0.0.0.0:4000/hoivakodit')
       .waitForElementVisible('.card-container')
       .end()
-  }
+  },
+  'Language selection is working' : function (browser) {
+    browser
+      .url('http://0.0.0.0:4000')
+      .waitForElementVisible('body')
+      .click('a[lang=sv-SV]')
+      .pause(300)
+      .assert.containsText('.title', 'Vårdhemmen i Esbo')
+      .click('a[lang=fi-FI]')
+      .pause(300)
+      .assert.containsText('.title', 'Espoon hoivakodit')
+      .click('a[lang=fi-FI]')
+      .pause(300)
+      .assert.containsText('.title', 'Espoon hoivakodit')
+      .end()
+  },
+  'Show nursinghomes button and selection is working' : function (browser) {
+    browser
+      .url('http://0.0.0.0:4000')
+      .waitForElementVisible('body')
+      .click('.location-picker-select button.button-dropdown')
+      .pause(200)
+      .assert.visible('.location-picker-select .button-dropdown-items')
+      .click('.location-picker-select button.button-dropdown')
+      .pause(200)
+      .click('.location-picker button.btn.landing-cta')
+      .pause(200)
+      .assert.urlEquals('http://0.0.0.0:4000/fi-FI/hoivakodit')
+      .end()
+  },
 };
