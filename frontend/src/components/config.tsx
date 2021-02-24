dd1b1067df3866aeca565181ec6368fb265fc431
mikkojamG <mikko.jamia@gofore.com>
2021-02-24 17:40:02 +0200

    Proxy working

--- frontend/src/components/config.tsx
+++ frontend/src/components/config.tsx
@@ -2,14 +2,14 @@
 const NODE_ENV: "development" | "production" | "test" = process.env.NODE_ENV;
 
 // URL to images in the public directory
-const publicFilesPortString = NODE_ENV === "development" ? ":4000" : "";
+const publicFilesPortString = NODE_ENV === "development" ? ":8000" : "";
 const PUBLIC_FILES_URL =
 	window.location.protocol +
 	"//" +
 	window.location.hostname +
 	publicFilesPortString;
 
-const apiPortString = NODE_ENV === "development" ? ":8000" : "";
+const apiPortString = NODE_ENV === "development" ? ":3000" : "";
 const API_URL =
 	window.location.protocol +
 	"//" +
