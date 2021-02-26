6348f973a4876d65f8aca7a70e441bd0bc3e9a75
mikkojamG <mikko.jamia@gofore.com>
2021-02-26 16:03:25 +0200

    Cleanup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./styles/global.scss";
 
-// eslint-disable-next-line no-undef
-console.log(process.env.KEYCLOAK_SECRET);
-
 ReactDOM.render(<App />, document.getElementById("root"));
